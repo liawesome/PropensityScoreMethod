b6c92ced0872ee4e261534102d81b729606a011e
Li <carriezha321@gmail.com>
2021-05-27 09:07:27 -0700

    Add files via upload

--- matching.py
+++ matching.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import sklearn
 from sklearn.neighbors import NearestNeighbors
-
+import scipy
 from scipy.stats import binom, gaussian_kde, ttest_ind, ranksums
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import seaborn as sns
 import os.path
+from itertools import chain
 
 
 # groups as 1/0 array
@@ -58,7 +59,7 @@ def rank_test(covariates, groups):
     return pd.DataFrame({'features': colnames, 'p-value': pvalues})
 
 
-def t_test(covariates, groups):
+def t_test(df, covariates, groups):
     """
     Two sample t test for the distribution of treatment and control covariates
 
@@ -75,17 +76,17 @@ def t_test(covariates, groups):
     -------
     A list of p-values, one for each column in covariates
     """
-    colnames = list(covariates.columns)
-    J = len(colnames)
+
+    J = len(covariates)
     pvalues = np.zeros(J)
     for j in range(J):
-        var = covariates[colnames[j]]
+        var = df[covariates[j]]
         res = ttest_ind(var[groups == 1], var[groups == 0])
         pvalues[j] = res.pvalue
 
     pvalues = pd.Series(pvalues)
     pvalues = round(pvalues, 3)
-    return pd.DataFrame({'features': colnames, 'p-value': pvalues})
+    return pd.DataFrame({'features': covariates, 'p-value': pvalues})
 
 def transform_data(df):
     features = list(df.columns)
@@ -102,11 +103,7 @@ def standardized_diff(df):
     the formula is smd = (Xbar_tret - Xbar_control)/ sqrt((s_tret^2+s_cont^2)/2)
     smd <0.2 good balance
     '''
-
-    features = df.columns.tolist()
-    features.remove('treatment')
-    features.remove('propensity')
-
+    features = transform_data(df)
     table = df.groupby('treatment').agg({feature: ['mean', 'std'] for feature in features})
     print(table.head())
 
@@ -125,21 +122,138 @@ def standardized_diff(df):
     return pd.DataFrame({'features': features, 'smd': feature_smds})
 
 
+def prop_test(col):
+    """
+    Performs a Chi-Square test of independence on <col>
+    See stats.chi2_contingency()
+    Parameters
+    ----------
+    col : str
+        Name of column on which the test should be performed
+    Returns
+    ______
+    dict
+        {'var': <col>,
+         'before': <pvalue before matching>,
+         'after': <pvalue after matching>}
+    """
+    
+    pval_before = round(scipy.stats.chi2_contingency(prep_prop_test(data,col))[1], 6)
+    pval_after = round(scipy.stats.chi2_contingency(prep_prop_test(matched_data,col))[1], 6)
+    return {'var':col, 'before':pval_before, 'after':pval_after}
 
-# def plotScores(groups, propensity, many=True):
-#     '''
-#     Plot density of propensity scores for each group before and after matching
-#
-#     Inputs: groups = treatment assignment, pre-matching
-#             propensity = propensity scores, pre-matching
-#             matches = output of Match or MatchMany
-#             many = indicator - True if one-many matching was done (default is True), otherwise False
-#     '''
-#
-#     # 1 Density distribution of propensity score (logic) broken down by treatment status
-#
-#
 
+
+def prep_prop_test(data, var):
+    """
+    Helper method for running chi-square contingency tests
+    Balances the counts of discrete variables with our groups
+    so that missing levels are replaced with 0.
+    i.e. if the test group has no records with x as a field
+    for a given column, make sure the count for x is 0
+    and not missing.
+    Parameters
+    ----------
+    data : pd.DataFrame()
+        Data to use for counting
+    var : str
+        Column to use within data
+    Returns
+    -------
+    list
+        A table (list of lists) of counts for all enumerated field within <var>
+        for test and control groups.
+    """
+    counts = data.groupby([var, yvar]).count().reset_index()
+    table = []
+    for t in (0, 1):
+        os_counts = counts[counts[yvar] ==t]\
+                                 .sort_values(var)
+        cdict = {}
+        for row in os_counts.iterrows():
+            row = row[1]
+            cdict[row[var]] = row[2]
+        table.append(cdict)
+    # fill empty keys as 0
+    all_keys = set(chain.from_iterable(table))
+    for d in table:
+        d.update((k, 0) for k in all_keys if k not in d)
+    ctable = [[i[k] for k in sorted(all_keys)] for i in table]
+    return ctable
+
+def chi_square(old_df,matched_df, old_df, col):
+    
+    title_str = '''
+    Proportional Difference (test-control) for {} Before and After Matching
+    Chi-Square Test for Independence p-value before | after:
+    {} | {}
+    '''
+    test_results = []
+
+    for col in cat_column:
+        dbefore = prep_plot(old_df, col, colname="before")
+        dafter = prep_plot(matched_df, col, colname="after")
+        df = dbefore.join(dafter)
+        test_results_i = prop_test(col)
+        test_results.append(test_results_i)
+
+    # plotting
+    df.plot.bar(alpha=.8)
+    plt.title(title_str.format(col, test_results_i["before"],
+               test_results_i["after"]))
+    lim = max(.09, abs(df).max().max()) + .01
+    plt.ylim((-lim, lim))
+    return pd.DataFrame(test_results)[['var', 'before', 'after']]
+
+
+def prep_plot(data, var,colname):
+    # treat, control 
+    c = data[data[yvar] == 0]
+    t = data[data[yvar] == 1]
+    # dummy var for counting
+    dummy = [i for i in t.columns if i not in \
+              (var, "match_id", "record_id", "weight")][0]
+    countt = t[[var, dummy]].groupby(var).count() / len(t)
+    countc = c[[var, dummy]].groupby(var).count() / len(c)
+    ret = (countt-countc).dropna()
+    ret.columns = [colname]
+    return ret    
+
+
+def plot_scores(old_df, match_df,args):
+    fig, ax =plt.subplots(1,2)
+    sns.distplot(old_df.propensity[old_df['treatment'] == 0], label='Control', ax=ax[0])
+    sns.distplot(old_df.propensity[old_df['treatment'] == 1], label='Treat', ax=ax[0])
+    ax[0].set_title('Before Matching')
+    ax[0].set(xlabel='Scores')
+    ax[0].set(ylabel='Density')
+    
+    sns.distplot(match_df.propensity[match_df['treatment'] == 0], label='Control',ax=ax[1])
+    sns.distplot(match_df.propensity[match_df['treatment'] == 1], label='Treat', ax=ax[1])
+    plt.legend(loc='upper right')
+    ax[1].set_title("After Matching")
+    ax[1].set(xlabel='Scores')
+    ax[1].legend(loc='center left',bbox_to_anchor=(1, 0.5))
+    fig.show()
+
+    
+    for arg in args:
+        fig, ax = plt.subplots(1,2)
+        fig.suptitle('Comparison of {} split by treatment status.'.format(arg))
+        sns.distplot(old_df[old_df['treatment'] == 0][arg], label="Control", ax=ax[0])
+        sns.distplot(old_df[old_df['treatment'] == 1][arg], label="treated", ax=ax[0])
+        ax[0].set_title('Before Matching')
+        ax[0].set(xlabel='Scores')
+        
+        sns.distplot(match_df[match_df['treatment'] == 0][arg], label="Control", ax=ax[1])
+        sns.distplot(match_df[match_df['treatment'] == 1][arg], label="treated", ax=ax[1])
+        ax[1].set_title("After Matching")
+        ax[1].set(xlabel='Scores')
+        ax[1].legend(loc='center left',bbox_to_anchor=(1, 0.5))
+        fig.show()
+        fig.savefig("{}_comparison.png".format(arg))      
+        
+        
 class Match:
         """
         Parameters
@@ -208,9 +322,17 @@ class Match:
             else:
                 raise ValueError('Invalid k value')
 
-        def test_balance(self, matched_df=None, old_df=None, test=None, many=True):
+        def test_balance(self, num_columns, catg_columns, matched_df=None, old_df=None, test=None, many=True):
+            ###########new#########
+            columns = num_columns + cat_columns
+            columns.append('treatment')
+            columns.append('propensity')
+            
+            matched_df= matched_df[columns]
+            old_df= old_df[columns]
+            
             if test is None:
-                test = ['smd', 't', 'rank', 'plot']
+                test = ['smd', 't', 'rank','chi-sq', 'plot']
 
             if many:
                 smd_b = list()
@@ -219,6 +341,8 @@ class Match:
                 rank = list()
                 for i in range(len(matched_df)):
                     if 'smd' in test:
+                        old_df = pd.get_dummies(old_df, columns=catg_columns)
+                        matched_df = pd.get_dummies(matched_df, columns=catg_columns)
                         smd_before = standardized_diff(old_df[i])
                         smd_after = standardized_diff(matched_df[i])
                         smd_b.append(smd_before)
@@ -246,19 +370,9 @@ class Match:
                         print(rank)
 
                     if 'plot' in test:
-                        fig, ax = plt.subplots(1, 2)
-                        fig.suptitle('Density distribution plots for propensity score and logit(propensity score).')
-                        predictions_logit = np.array([np.logit(xi) for xi in matched_df[i]['propensity']])
-                        sns.kdeplot(x= matched_df[i]['propensity'], hue="treatment", ax=ax[0])
-                        ax[0].set_title('Propensity Score')
-
-                        sns.kdeplot(x=predictions_logit , hue="treatment", ax=ax[1])
-                        ax[1].axvline(-0.4, ls='--')
-                        ax[1].set_title('Logit of Propensity Score')
-                        plt.savefig('propensity score.png')
-                        plt.show()
-
-                        savefig('propensity_{:03d}.jpg'.format(i))
+                        print('For each matched data'.format(i))
+                        plot_scores(old_df, matched_df, args)
+                        
 
             else:
                 if 'smd' in test:
@@ -270,7 +384,7 @@ class Match:
 
                 if 't' in test:
                     features = transform_data(matched_df)
-                    ttest = t_test(matched_df[features], matched_df['treatment'])
+                    ttest = t_test(matched_df, features, matched_df['treatment'])
                     print("The test result for t-test:")
                     print(ttest)
 
@@ -282,35 +396,20 @@ class Match:
                     rtest = rank_test(features, matched_df['treatment'])
                     print("The test result for Wilcoxon-test:")
                     print(rtest)
-
+                    
                 if 'plot' in test:
-                    fig, ax = plt.subplots(1, 2)
-                    fig.suptitle('Density distribution plots for propensity score and logit(propensity score).')
-                    predictions_logit = np.array([np.logit(xi) for xi in matched_df['propensity']])
-                    sns.kdeplot(x=matched_df['propensity'], hue="treatment", ax=ax[0])
-                    ax[0].set_title('Propensity Score')
+                    args = num_column
+                    plot_scores(old_df, matched_df, args)
+                    
+                    
+                    
 
-                    sns.kdeplot(x=predictions_logit, hue="treatment", ax=ax[1])
-                    ax[1].axvline(-0.4, ls='--')
-                    ax[1].set_title('Logit of Propensity Score')
-                    plt.savefig('propensity score.png')
-                    plt.show()
+    
 
-                    savefig('propensity_{:03d}.jpg'.format(i))
 
-    plt.rcParams['figure.figsize'] = 8, 6
-    plt.rcParams['font.size'] = 12
-
-    sns.distplot(neg_pscore, label='control')
-    sns.distplot(pos_pscore, label='treatment')
-    plt.xlim(0, 1)
-    plt.title('Propensity Score Distribution of Control vs Treatment')
-    plt.ylabel('Density')
-    plt.xlabel('Scores')
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
+def logit(x):
+    propensity = np.log(x / (1 - x))
+    return x
 
 
 
